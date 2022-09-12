29c4e5ee716e9e54a0db57a278bfd8a52f7b4ba2
Umbrella1234 <rozimark96@mail.ru>
2022-09-12 19:01:39 +0200

    added an icon

--- src/features/employees/api/getEmployees.ts
+++ src/features/employees/api/getEmployees.ts
@@ -3,14 +3,14 @@ import { EmployeesMap } from "../../../types/entities/Employee";
 
 const FAKE_DELAY_MS = 2000;
 
-// creates promises that resolve with provided function argument after a delay
-const delayedPromiseFactory = <T>(resolveValue: T) =>
+// creates a promise that resolves with provided function argument after a delay
+const delayedPromiseFactoryFn = <T>(resolveValue: T) =>
   new Promise<T>((resolve) =>
     delay(() => resolve(resolveValue), FAKE_DELAY_MS)
   );
 
 export const getEmployees = () =>
-  delayedPromiseFactory<EmployeesMap>({
+  delayedPromiseFactoryFn<EmployeesMap>({
     1: {
       avatar: null,
       email: "haha@mail.ru",
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -4,9 +4,20 @@ import { Text, View } from "react-native";
 import { getEmployees } from "../../features/employees/api/getEmployees";
 import { Queries } from "../../queryClient";
 import { EmployeesArr, EmployeesMap } from "../../types/entities/Employee";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-const StyledText = styled.Text`
-  color: red;
+const StyledEmployee = styled.View`
+  display: flex;
+  flex-direction: row;
+  padding: 15px;
+`;
+
+const StyledEmployeeInfo = styled.Text`
+  display: flex;
+`;
+
+const StyledEmployeeMenuIcon = styled(MaterialCommunityIcons)`
+  margin-left: auto;
 `;
 
 export const Home = () => {
@@ -21,16 +32,20 @@ export const Home = () => {
   return (
     <View>
       {isLoading && <Text>Loading wait plez...</Text>}
-      <StyledText>{data?.length}</StyledText>
       {isSuccess && (
         <>
           {data.map(({ avatar, email, name, surname, id }) => {
             return (
-              <View>
-                <StyledText>
+              <StyledEmployee>
+                <StyledEmployeeInfo>
                   {name} {surname} {email}
-                </StyledText>
-              </View>
+                </StyledEmployeeInfo>
+                <StyledEmployeeMenuIcon
+                  name="menu"
+                  size={28}
+                  color="#010101"
+                ></StyledEmployeeMenuIcon>
+              </StyledEmployee>
             );
           })}
         </>
